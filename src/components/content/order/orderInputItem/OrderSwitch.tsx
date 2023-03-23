c4aa65fb9afb7ef82ecf86ae0716632000efb63f
xieyichao <xieyichao@huawei.com>
2023-03-23 20:35:15 +0800

    Add OrderSubmitPage.

---
+++ src/components/content/order/orderInputItem/OrderSwitch.tsx
@@ -0,0 +1,31 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * SPDX-FileCopyrightText: Huawei Inc.
+ */
+
+import { Form, Switch } from 'antd';
+import { DeployParam, SwitchOnChangeHandler } from './OrderCommon';
+
+export function OrderSwitch({
+    item,
+    onChangeHandler,
+}: {
+    item: DeployParam;
+    onChangeHandler: SwitchOnChangeHandler;
+}): JSX.Element {
+    return (
+        <div className={'order-param-item-row'}>
+            <div className={'order-param-item-left'} />
+            <div className={'order-param-item-content'}>
+                <Form.Item
+                    name={item.name}
+                    label={item.name + ':  (' + item.description + ')'}
+                    rules={[{ type: 'boolean' }]}
+                >
+                    <Switch onChange={onChangeHandler} defaultChecked={false} />
+                </Form.Item>
+            </div>
+            <div className={'order-param-item-right'} />
+        </div>
+    );
+}
