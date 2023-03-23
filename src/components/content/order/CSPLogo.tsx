cea0dab8ee34a2b54ccffa3d9562a845adccd90a
Alice1319 <alice_lai1319@outlook.com>
2023-03-23 20:35:06 +0800

    update service page one.

---
+++ src/components/content/order/CSPLogo.tsx
@@ -0,0 +1,11 @@
+import HuaWeiLogo from '../../../img/Huawei_Logo.png';
+import AlibabaLogo from '../../../img/Alibaba_Logo.png';
+import AzureLogo from '../../../img/Microsoft_Azure_Logo.png';
+import AWSLogo from '../../../img/Aws_Logo.png';
+import XpanseLogo from '../../../img/Eclipse_Xpanse_Logo.png';
+
+export { HuaWeiLogo };
+export { AlibabaLogo };
+export { AzureLogo };
+export { AWSLogo };
+export { XpanseLogo };
---
+++ src/components/content/order/Services.tsx
@@ -0,0 +1,74 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * SPDX-FileCopyrightText: Huawei Inc.
+ */
+
+import { FormOutlined } from '@ant-design/icons';
+import '../../../styles/service_order.css';
+import { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { createServicePageRoute } from '../../utils/constants';
+import { serviceVendorApi } from '../../../xpanse-api/xpanseRestApiClient';
+
+function Services(): JSX.Element {
+    const [services, setServices] = useState<{ name: string; content: string; icon: string }[]>([]);
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const onClicked = function (cfg: string) {
+        navigate(
+            createServicePageRoute
+                .concat('?serviceName=', location.hash.split('#')[1])
+                .concat('&name=', cfg.replace(' ', ''))
+        );
+    };
+
+    useEffect(() => {
+        const categoryName = location.hash.split('#')[1];
+        if (!categoryName) {
+            return;
+        }
+        serviceVendorApi.listRegisteredServicesTree(categoryName).then((rsp) => {
+            if (rsp.length > 0) {
+                let serviceList: { name: string; content: string; icon: string }[] = [];
+                rsp.forEach((item) => {
+                    let serviceItem = {
+                        name: item.name || '',
+                        content: item.versions[0].cloudProvider[0].details[0].description,
+                        icon: item.versions[0].cloudProvider[0].details[0].icon,
+                    };
+                    serviceList.push(serviceItem);
+                });
+                setServices(serviceList);
+            } else {
+                return;
+            }
+        });
+    }, [location]);
+
+    return (
+        <div className={'services-content'}>
+            <div className={'content-title'}>
+                <FormOutlined />
+                &nbsp;Select Service
+            </div>
+            <div className={'services-content-body'}>
+                {services.map((item, index) => {
+                    return (
+                        <div key={index} className={'service-type-option-detail'} onClick={(e) => onClicked(item.name)}>
+                            <div className='service-type-option-image'>
+                                <img className='service-type-option-service-icon' src={item.icon} alt={'App'} />
+                            </div>
+                            <div className='service-type-option-info'>
+                                <span className='service-type-option'>{item.name}</span>
+                                <span className='service-type-option-description'>{item.content}</span>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    );
+}
+
+export default Services;
