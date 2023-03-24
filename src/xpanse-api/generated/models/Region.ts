5279f17fc6964598051163ff50f6c45a6cd77e79
WangLiNaruto <wl-naruto@outlook.com>
2023-03-24 16:17:32 +0800

    update area and xpanse Api

--- src/xpanse-api/generated/models/Region.ts
+++ src/xpanse-api/generated/models/Region.ts
@@ -19,7 +19,14 @@
  * The regions of the Cloud Service Provider
  */
 export class Region {
+    /**
+     * The name of the Region
+     */
     'name': string;
+
+    /**
+     * The area which the region belongs to, such as Asia, Europe, Africa
+     */
     'area'?: string;
 
     static readonly discriminator: string | undefined = undefined;
