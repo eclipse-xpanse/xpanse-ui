8ce93ad425852411f7d93a0006a2ab9ad7bc5518
swaroopar <swaroop.akulu.raghupathy@huawei.com>
2023-05-02 11:47:37 +0200

    show error when API returns error

--- src/components/content/order/Services.tsx
+++ src/components/content/order/Services.tsx
@@ -13,12 +13,14 @@ import { Badge, Space } from 'antd';
 import { sortVersion } from '../../utils/Sort';
 import { ServicesAvailableService, ServiceVo, VersionOclVo } from '../../../xpanse-api/generated';
 import ServicesSkeleton from './ServicesSkeleton';
+import ServicesLoadingError from './ServicesLoadingError';
 
 function Services(): JSX.Element {
     const [services, setServices] = useState<{ name: string; content: string; icon: string; latestVersion: string }[]>(
         []
     );
     const [isServicesLoaded, setIsServicesLoaded] = useState<boolean>(false);
+    const [isServicesLoadSuccessful, setIsServicesLoadSuccessful] = useState<boolean>(true);
     const navigate = useNavigate();
     const location = useLocation();
 
@@ -42,79 +44,93 @@ function Services(): JSX.Element {
     useEffect(() => {
         setIsServicesLoaded(false);
         const categoryName = location.hash.split('#')[1] as ServiceVo.category;
-        void ServicesAvailableService.getAvailableServicesTree(categoryName).then((rsp) => {
-            const serviceList: { name: string; content: string; icon: string; latestVersion: string }[] = [];
-            if (rsp.length > 0) {
-                rsp.forEach((item) => {
-                    const serviceItem = {
-                        name: item.name || '',
-                        content: item.versions[0].cloudProvider[0].details[0].description,
-                        icon: item.versions[0].cloudProvider[0].details[0].icon,
-                        latestVersion: sortVersion(getVersions(item.versions))[0],
-                    };
-                    serviceList.push(serviceItem);
-                });
-                setServices(serviceList);
+        void ServicesAvailableService.getAvailableServicesTree(categoryName)
+            .then((rsp) => {
+                const serviceList: { name: string; content: string; icon: string; latestVersion: string }[] = [];
+                if (rsp.length > 0) {
+                    rsp.forEach((item) => {
+                        const serviceItem = {
+                            name: item.name || '',
+                            content: item.versions[0].cloudProvider[0].details[0].description,
+                            icon: item.versions[0].cloudProvider[0].details[0].icon,
+                            latestVersion: sortVersion(getVersions(item.versions))[0],
+                        };
+                        serviceList.push(serviceItem);
+                    });
+                    setServices(serviceList);
+                    setIsServicesLoaded(true);
+                    setIsServicesLoadSuccessful(true);
+                } else {
+                    setServices(serviceList);
+                    setIsServicesLoaded(true);
+                    setIsServicesLoadSuccessful(true);
+                }
+            })
+            .catch(() => {
+                setServices([]);
                 setIsServicesLoaded(true);
-            } else {
-                setServices(serviceList);
-                setIsServicesLoaded(true);
-            }
-        });
+                setIsServicesLoadSuccessful(false);
+            });
     }, [location]);
 
     return (
         <>
             {' '}
-            {isServicesLoaded ? (
-                services.length > 0 ? (
-                    <div className={'services-content'}>
-                        <div className={'content-title'}>
-                            <FormOutlined />
-                            &nbsp;Select Service
-                        </div>
+            {isServicesLoadSuccessful ? (
+                isServicesLoaded ? (
+                    services.length > 0 ? (
+                        <div className={'services-content'}>
+                            <div className={'content-title'}>
+                                <FormOutlined />
+                                &nbsp;Select Service
+                            </div>
 
-                        <div className={'services-content-body'}>
-                            {services.map((item, index) => {
-                                return (
-                                    <Row key={index}>
-                                        <Col span={8} className={'services-content-body-col'}>
-                                            <Space direction='vertical' size='middle'>
-                                                <Badge.Ribbon text={item.latestVersion}>
-                                                    <div
-                                                        key={index}
-                                                        className={'service-type-option-detail'}
-                                                        onClick={() => onSelectService(item.name, item.latestVersion)}
-                                                    >
-                                                        <div className='service-type-option-image'>
-                                                            <img
-                                                                className='service-type-option-service-icon'
-                                                                src={item.icon}
-                                                                alt={'App'}
-                                                            />
+                            <div className={'services-content-body'}>
+                                {services.map((item, index) => {
+                                    return (
+                                        <Row key={index}>
+                                            <Col span={8} className={'services-content-body-col'}>
+                                                <Space direction='vertical' size='middle'>
+                                                    <Badge.Ribbon text={item.latestVersion}>
+                                                        <div
+                                                            key={index}
+                                                            className={'service-type-option-detail'}
+                                                            onClick={() =>
+                                                                onSelectService(item.name, item.latestVersion)
+                                                            }
+                                                        >
+                                                            <div className='service-type-option-image'>
+                                                                <img
+                                                                    className='service-type-option-service-icon'
+                                                                    src={item.icon}
+                                                                    alt={'App'}
+                                                                />
+                                                            </div>
+                                                            <div className='service-type-option-info'>
+                                                                <span className='service-type-option'>{item.name}</span>
+                                                                <span className='service-type-option-description'>
+                                                                    {item.content}
+                                                                </span>
+                                                            </div>
                                                         </div>
-                                                        <div className='service-type-option-info'>
-                                                            <span className='service-type-option'>{item.name}</span>
-                                                            <span className='service-type-option-description'>
-                                                                {item.content}
-                                                            </span>
-                                                        </div>
-                                                    </div>
-                                                </Badge.Ribbon>
-                                            </Space>
-                                        </Col>
-                                    </Row>
-                                );
-                            })}
+                                                    </Badge.Ribbon>
+                                                </Space>
+                                            </Col>
+                                        </Row>
+                                    );
+                                })}
+                            </div>
                         </div>
-                    </div>
+                    ) : (
+                        <div className={'service-blank-class'}>
+                            <Empty description={'No services available.'} />
+                        </div>
+                    )
                 ) : (
-                    <div className={'service-blank-class'}>
-                        <Empty description={'No services available.'} />
-                    </div>
+                    <ServicesSkeleton />
                 )
             ) : (
-                <ServicesSkeleton />
+                <ServicesLoadingError />
             )}
         </>
     );
