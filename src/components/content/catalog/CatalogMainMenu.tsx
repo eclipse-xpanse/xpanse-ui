445f3bb29f5fe1a3be1f50efb2d155cb10c45f40
swaroopar <swaroop.akulu.raghupathy@huawei.com>
2023-07-24 16:53:00 +0200

    load services only when category is selected

---
+++ src/components/content/catalog/CatalogMainMenu.tsx
@@ -0,0 +1,26 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * SPDX-FileCopyrightText: Huawei Inc.
+ */
+
+import { useLocation } from 'react-router-dom';
+import { ServiceVo } from '../../../xpanse-api/generated';
+import CategoryCatalog from './CategoryCatalog';
+
+export default function CatalogMainPage(): JSX.Element {
+    const location = useLocation();
+    const category = getCategory();
+
+    function getCategory(): ServiceVo.category | undefined {
+        if (location.hash.split('#').length > 1) {
+            return location.hash.split('#')[1] as ServiceVo.category;
+        }
+        return undefined;
+    }
+
+    if (category) {
+        return <CategoryCatalog category={category} />;
+    } else {
+        return <></>;
+    }
+}
