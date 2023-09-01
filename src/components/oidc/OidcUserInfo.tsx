d0e31d942d388ac4ead1808f0c95b27ee6e98400
baixinsui <baixinsui@outlook.com>
2023-09-01 15:14:58 +0800

    Add scope to get metadata of user

---
+++ src/components/oidc/OidcUserInfo.tsx
@@ -0,0 +1,8 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * SPDX-FileCopyrightText: Huawei Inc.
+ */
+
+export const usernameKey = 'name';
+export const defaultRole = 'user';
+export const grantedRolesKey = 'urn:zitadel:iam:org:project:roles';
