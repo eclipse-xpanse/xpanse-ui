3b02986863490b155a2e7e371c1a3d942efa5457
swaroopar <swaroop.akulu.raghupathy@huawei.com>
2023-09-13 16:24:52 +0200

    update logout URL for new URIs

--- src/components/content/login/Logout.tsx
+++ src/components/content/login/Logout.tsx
@@ -7,7 +7,7 @@ import { PoweroffOutlined } from '@ant-design/icons';
 import { Button } from 'antd';
 import { useOidc } from '@axa-fr/react-oidc';
 import React from 'react';
-import { createServicePageRoute, homePageRoute } from '../../utils/constants';
+import { createServicePageRoute, homePageRoute, registerPageRoute } from '../../utils/constants';
 function Logout(): React.JSX.Element {
     const { logout } = useOidc();
 
@@ -15,6 +15,9 @@ function Logout(): React.JSX.Element {
         if (window.location.pathname.startsWith(createServicePageRoute)) {
             // Create service URL contains dynamic url. So, redirect back to the home page.
             return homePageRoute;
+        }
+        if (window.location.pathname.startsWith(registerPageRoute)) {
+            return registerPageRoute;
         }
         return window.location.href.replace(window.location.hash, '');
     }
