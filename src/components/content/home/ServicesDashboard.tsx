74df6dacb48ddce1c5f04e2c73b7311262afde69
swaroopar <swaroop.akulu.raghupathy@huawei.com>
2023-11-07 08:23:42 +0000

    display services dashboard even when no services are displayed

--- src/components/content/home/ServicesDashboard.tsx
+++ src/components/content/home/ServicesDashboard.tsx
@@ -16,6 +16,10 @@ import serviceDeploymentState = ServiceVo.serviceDeploymentState;
 export function ServicesDashboard(): React.JSX.Element {
     const listDeployedServicesQuery = useListDeployedServicesQuery();
     const navigate = useNavigate();
+    let successfulDeploymentsCount: number = 0;
+    let failedDeploymentsCount: number = 0;
+    let successfulDestroysCount: number = 0;
+    let failedDestroysCount: number = 0;
 
     if (listDeployedServicesQuery.isError) {
         return <ServicesDashboardError error={listDeployedServicesQuery.error} />;
@@ -26,10 +30,6 @@ export function ServicesDashboard(): React.JSX.Element {
     }
 
     if (listDeployedServicesQuery.data.length > 0) {
-        let successfulDeploymentsCount: number = 0;
-        let failedDeploymentsCount: number = 0;
-        let successfulDestroysCount: number = 0;
-        let failedDestroysCount: number = 0;
         listDeployedServicesQuery.data.forEach((serviceItem: ServiceVo) => {
             if (serviceItem.serviceDeploymentState === ServiceVo.serviceDeploymentState.DEPLOYMENT_SUCCESSFUL) {
                 successfulDeploymentsCount++;
@@ -44,85 +44,83 @@ export function ServicesDashboard(): React.JSX.Element {
                 failedDestroysCount++;
             }
         });
-
-        const getMyServicesRedirectionUrl = (serviceState: ServiceVo.serviceDeploymentState) => {
-            navigate({
-                pathname: myServicesRoute,
-                search: createSearchParams({
-                    serviceState: serviceState.valueOf(),
-                }).toString(),
-            });
-        };
-
-        return (
-            <>
-                <Card title='Services Dashboard' bordered={true}>
-                    <Row gutter={16}>
-                        <Col span={12}>
-                            <div
-                                onClick={() => {
-                                    getMyServicesRedirectionUrl(serviceDeploymentState.DEPLOYMENT_SUCCESSFUL);
-                                }}
-                            >
-                                <Statistic
-                                    title='Successful Deployments'
-                                    loading={listDeployedServicesQuery.isLoading}
-                                    value={successfulDeploymentsCount}
-                                    valueStyle={{ color: '#3f8600' }}
-                                    className={'clickable-dashboard-links'}
-                                />
-                            </div>
-                        </Col>
-                        <Col span={12}>
-                            <div
-                                onClick={() => {
-                                    getMyServicesRedirectionUrl(serviceDeploymentState.DEPLOYMENT_FAILED);
-                                }}
-                            >
-                                <Statistic
-                                    title='Failed Deployments'
-                                    value={failedDeploymentsCount}
-                                    loading={listDeployedServicesQuery.isLoading}
-                                    valueStyle={{ color: '#cf1322' }}
-                                    className={'clickable-dashboard-links'}
-                                />
-                            </div>
-                        </Col>
-                        <Col span={12}>
-                            <div
-                                onClick={() => {
-                                    getMyServicesRedirectionUrl(serviceDeploymentState.DESTROY_SUCCESSFUL);
-                                }}
-                            >
-                                <Statistic
-                                    title='Successful Destroys'
-                                    loading={listDeployedServicesQuery.isLoading}
-                                    value={successfulDestroysCount}
-                                    valueStyle={{ color: '#3f8600' }}
-                                    className={'clickable-dashboard-links'}
-                                />
-                            </div>
-                        </Col>
-                        <Col span={12}>
-                            <div
-                                onClick={() => {
-                                    getMyServicesRedirectionUrl(serviceDeploymentState.DESTROY_FAILED);
-                                }}
-                            >
-                                <Statistic
-                                    title='Failed Destroys'
-                                    value={failedDestroysCount}
-                                    loading={listDeployedServicesQuery.isLoading}
-                                    valueStyle={{ color: '#cf1322' }}
-                                    className={'clickable-dashboard-links'}
-                                />
-                            </div>
-                        </Col>
-                    </Row>
-                </Card>
-            </>
-        );
     }
 
-    return <></>;
+    const getMyServicesRedirectionUrl = (serviceState: ServiceVo.serviceDeploymentState) => {
+        navigate({
+            pathname: myServicesRoute,
+            search: createSearchParams({
+                serviceState: serviceState.valueOf(),
+            }).toString(),
+        });
+    };
+
+    return (
+        <>
+            <Card title='Services Dashboard' bordered={true}>
+                <Row gutter={16}>
+                    <Col span={12}>
+                        <div
+                            onClick={() => {
+                                getMyServicesRedirectionUrl(serviceDeploymentState.DEPLOYMENT_SUCCESSFUL);
+                            }}
+                        >
+                            <Statistic
+                                title='Successful Deployments'
+                                loading={listDeployedServicesQuery.isLoading}
+                                value={successfulDeploymentsCount}
+                                valueStyle={{ color: '#3f8600' }}
+                                className={'clickable-dashboard-links'}
+                            />
+                        </div>
+                    </Col>
+                    <Col span={12}>
+                        <div
+                            onClick={() => {
+                                getMyServicesRedirectionUrl(serviceDeploymentState.DEPLOYMENT_FAILED);
+                            }}
+                        >
+                            <Statistic
+                                title='Failed Deployments'
+                                value={failedDeploymentsCount}
+                                loading={listDeployedServicesQuery.isLoading}
+                                valueStyle={{ color: '#cf1322' }}
+                                className={'clickable-dashboard-links'}
+                            />
+                        </div>
+                    </Col>
+                    <Col span={12}>
+                        <div
+                            onClick={() => {
+                                getMyServicesRedirectionUrl(serviceDeploymentState.DESTROY_SUCCESSFUL);
+                            }}
+                        >
+                            <Statistic
+                                title='Successful Destroys'
+                                loading={listDeployedServicesQuery.isLoading}
+                                value={successfulDestroysCount}
+                                valueStyle={{ color: '#3f8600' }}
+                                className={'clickable-dashboard-links'}
+                            />
+                        </div>
+                    </Col>
+                    <Col span={12}>
+                        <div
+                            onClick={() => {
+                                getMyServicesRedirectionUrl(serviceDeploymentState.DESTROY_FAILED);
+                            }}
+                        >
+                            <Statistic
+                                title='Failed Destroys'
+                                value={failedDestroysCount}
+                                loading={listDeployedServicesQuery.isLoading}
+                                valueStyle={{ color: '#cf1322' }}
+                                className={'clickable-dashboard-links'}
+                            />
+                        </div>
+                    </Col>
+                </Row>
+            </Card>
+        </>
+    );
 }
