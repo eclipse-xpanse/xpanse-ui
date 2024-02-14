34bf021528efbca970b01ac2b4482503ca631c56
swaroopar <swaroop.akulu.raghupathy@huawei.com>
2024-02-14 08:47:56 +0000

    remove redundant useEffect hook in user roles menu
    Signed-off-by: swaroopar <swaroop.akulu.raghupathy@huawei.com>

--- src/components/layouts/header/HeaderUserRoles.tsx
+++ src/components/layouts/header/HeaderUserRoles.tsx
@@ -4,12 +4,11 @@
  */
 
 import { Divider, Dropdown, MenuProps, Space, theme } from 'antd';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { UserOutlined } from '@ant-design/icons';
 import { homePageRoute } from '../../utils/constants';
 import { useLocation, useNavigate } from 'react-router-dom';
 import Logout from '../../content/login/Logout';
-import { useOidcIdToken } from '@axa-fr/react-oidc';
 import { allowRoleList, getRolesOfUser, getUserName } from '../../oidc/OidcConfig';
 import { OidcIdToken } from '@axa-fr/react-oidc/dist/ReactOidc';
 import { useCurrentUserRoleStore } from './useCurrentRoleStore';
@@ -32,73 +31,65 @@ function getItem(
     } as MenuItem;
 }
 
-export const HeaderUserRoles = (): React.JSX.Element => {
+export function HeaderUserRoles({ oidcIdToken }: { oidcIdToken: OidcIdToken }): React.JSX.Element {
     const navigate = useNavigate();
     const location = useLocation();
     const { useToken } = theme;
     const { token } = useToken();
-    const [currentUser, setCurrentUser] = useState<string>('');
-    const [menuProps, setMenuProps] = useState<MenuProps | undefined>(undefined);
-    const oidcIdToken: OidcIdToken = useOidcIdToken();
+    let menuProps: MenuProps | undefined = undefined;
     const currentRole = useCurrentUserRoleStore((state) => state.currentUserRole);
     const updateCurrentUserRole = useCurrentUserRoleStore((state) => state.addCurrentUserRole);
+    const userName: string = getUserName(oidcIdToken.idTokenPayload as object);
+    const roleList: string[] = getRolesOfUser(oidcIdToken.idTokenPayload as object);
+    let updatedRole: string | undefined = undefined;
+    // we must take the previously selected role if available.
+    if (currentRole !== undefined && roleList.includes(currentRole)) {
+        updatedRole = currentRole;
+    } else {
+        // if no role is already cached, then take the first valid role from the list of available roles for the user.
+        roleList.forEach((role) => {
+            if (allowRoleList.includes(role)) {
+                updateCurrentUserRole(role);
+                updatedRole = role;
+                return;
+            }
+        });
+    }
 
-    useEffect(() => {
-        const userName: string = getUserName(oidcIdToken.idTokenPayload as object);
-        const roleList: string[] = getRolesOfUser(oidcIdToken.idTokenPayload as object);
-        let updatedRole: string | undefined = undefined;
-        setCurrentUser(userName);
+    if (roleList.length !== 0 && updatedRole) {
+        const subItems: MenuItem[] = [];
+        roleList.forEach((item) => {
+            const menuItem: MenuItem = { label: item, key: item };
+            subItems.push(menuItem);
+        });
 
-        // we must take the previously selected role if available.
-        if (currentRole !== undefined && roleList.includes(currentRole)) {
-            updatedRole = currentRole;
-        } else {
-            // if no role is already cached, then take the first valid role from the list of available roles for the user.
-            roleList.forEach((role) => {
-                if (allowRoleList.includes(role)) {
-                    updateCurrentUserRole(role);
-                    updatedRole = role;
-                    return;
-                }
-            });
-        }
+        const items: MenuItem[] = [getItem('Switch Role', 'switchRole', null, subItems)];
+        const menuStyle = {
+            boxShadow: 'none',
+        };
 
-        if (roleList.length !== 0 && updatedRole) {
-            const subItems: MenuItem[] = [];
-            roleList.forEach((item) => {
-                const menuItem: MenuItem = { label: item, key: item };
-                subItems.push(menuItem);
-            });
+        const handleMenuClick: MenuProps['onClick'] = (value) => {
+            updateCurrentUserRole(value.key);
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+            const origin: string = (location.state?.from?.pathname as string) || homePageRoute;
+            navigate(origin);
+        };
 
-            const items: MenuItem[] = [getItem('Switch Role', 'switchRole', null, subItems)];
-            const menuStyle = {
-                boxShadow: 'none',
-            };
-
-            setMenuProps({
-                items,
-                onClick: handleMenuClick,
-                selectable: roleList.length > 1,
-                selectedKeys: [updatedRole],
-                defaultOpenKeys: ['switchRole'],
-                mode: 'vertical',
-                style: menuStyle,
-            });
-        }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [oidcIdToken.idTokenPayload, currentRole]);
+        menuProps = {
+            items,
+            onClick: handleMenuClick,
+            selectable: roleList.length > 1,
+            selectedKeys: [updatedRole],
+            defaultOpenKeys: ['switchRole'],
+            mode: 'vertical',
+            style: menuStyle,
+        };
+    }
 
     const contentStyle = {
         backgroundColor: token.colorBgElevated,
         borderRadius: token.borderRadiusLG,
         boxShadow: token.boxShadowSecondary,
-    };
-
-    const handleMenuClick: MenuProps['onClick'] = (value) => {
-        updateCurrentUserRole(value.key);
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-        const origin: string = (location.state?.from?.pathname as string) || homePageRoute;
-        navigate(origin);
     };
 
     return (
@@ -123,10 +114,10 @@ export const HeaderUserRoles = (): React.JSX.Element => {
                 >
                     <Space>
                         <UserOutlined />
-                        {currentUser} &nbsp;
+                        {userName} &nbsp;
                     </Space>
                 </a>
             </Dropdown>
         </Space>
     );
-};
+}
