19d5640454b5350b4b36d93afee406b1dc75835c
Alice1319 <alice_lai1319@outlook.com>
2024-02-22 17:51:21 +0800

    Add service vendor contact details in myServices.

--- src/components/content/common/ocl/ContactDetailsText.tsx
+++ src/components/content/common/ocl/ContactDetailsText.tsx
@@ -4,10 +4,9 @@
  */
 
 import { ServiceProviderContactDetails } from '../../../../xpanse-api/generated';
-import YAML from 'yaml';
-import { Button, Popover } from 'antd';
+import { Button, Popover, Typography } from 'antd';
 import React from 'react';
-import { ContactsOutlined } from '@ant-design/icons';
+import { ContactsOutlined, GlobalOutlined, MailOutlined, MessageOutlined, PhoneOutlined } from '@ant-design/icons';
 import { ContactDetailsShowType } from './ContactDetailsShowType';
 
 export function ContactDetailsText({
@@ -17,25 +16,108 @@ export function ContactDetailsText({
     serviceProviderContactDetails: ServiceProviderContactDetails;
     showFor: ContactDetailsShowType;
 }): React.JSX.Element {
-    const filterNullProperties = (contactDetails: ServiceProviderContactDetails): ServiceProviderContactDetails => {
-        // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-        return Object.fromEntries(Object.entries(contactDetails).filter(([_, value]) => value !== null));
-    };
+    return (
+        <Popover
+            content={<pre>{convertRecordToList(serviceProviderContactDetails)}</pre>}
+            title={'Contact Details'}
+            trigger='hover'
+        >
+            <Button className={'ocl-data-hover'} type={'link'}>
+                <ContactsOutlined />
+                {showFor === ContactDetailsShowType.Order ? ' Contact Service Vendor ' : ' support '}
+            </Button>
+        </Popover>
+    );
+}
 
-    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-    if (serviceProviderContactDetails) {
-        const yamlDocument = new YAML.Document();
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-expect-error
-        yamlDocument.contents = filterNullProperties(serviceProviderContactDetails);
-        return (
-            <Popover content={<pre>{yamlDocument.toString()}</pre>} title={'Contact Details'} trigger='hover'>
-                <Button className={'ocl-data-hover'} type={'link'}>
-                    <ContactsOutlined />
-                    {showFor === ContactDetailsShowType.Order ? ' Contact Service Vendor ' : ' support '}
-                </Button>
-            </Popover>
+function convertRecordToList(serviceProviderContactDetails: ServiceProviderContactDetails): React.JSX.Element {
+    const content: React.JSX.Element[] = [];
+    if (serviceProviderContactDetails.email) {
+        content.push(
+            <>
+                <div className={'ocl-contact-details-class'}>
+                    <MailOutlined />
+                    {getEmailPropertyValue(serviceProviderContactDetails.email)}
+                </div>
+            </>
         );
     }
-    return <></>;
+    if (serviceProviderContactDetails.phone) {
+        content.push(
+            <>
+                <div className={'ocl-contact-details-class'}>
+                    <PhoneOutlined />
+                    {getPhonePropertyValue(serviceProviderContactDetails.phone)}
+                </div>
+            </>
+        );
+    }
+    if (serviceProviderContactDetails.chat) {
+        content.push(
+            <>
+                <div className={'ocl-contact-details-class'}>
+                    <MessageOutlined />
+                    {getPropertyValue(serviceProviderContactDetails.chat)}
+                </div>
+            </>
+        );
+    }
+    if (serviceProviderContactDetails.website) {
+        content.push(
+            <>
+                <div className={'ocl-contact-details-class'}>
+                    <GlobalOutlined />
+                    {getPropertyValue(serviceProviderContactDetails.website)}
+                </div>
+            </>
+        );
+    }
+    return <>{content}</>;
+}
+
+function getEmailPropertyValue(emailValues: string[]) {
+    const { Paragraph } = Typography;
+    const valueList: React.JSX.Element[] = [];
+
+    emailValues.forEach((email) => {
+        valueList.push(
+            <Paragraph key={email} style={{ margin: 0 }}>
+                <span>&nbsp;&nbsp;- </span>
+                <a href={`mailto:${email}`}>{email}</a>
+            </Paragraph>
+        );
+    });
+    return <>{valueList}</>;
+}
+
+function getPhonePropertyValue(phoneValues: string[]) {
+    const { Paragraph } = Typography;
+    const valueList: React.JSX.Element[] = [];
+
+    phoneValues.forEach((phone) => {
+        valueList.push(
+            <Paragraph key={phone} style={{ margin: 0 }}>
+                <span>&nbsp;&nbsp;- </span>
+                {phone}
+            </Paragraph>
+        );
+    });
+    return <>{valueList}</>;
+}
+
+function getPropertyValue(values: string[]) {
+    const { Paragraph } = Typography;
+    const valueList: React.JSX.Element[] = [];
+
+    values.forEach((value) => {
+        valueList.push(
+            <Paragraph key={value} style={{ margin: 0 }}>
+                <span>&nbsp;&nbsp;- </span>
+                <a href={value} target='_blank' rel='noopener noreferrer'>
+                    {value}
+                </a>
+            </Paragraph>
+        );
+    });
+    return <>{valueList}</>;
 }
