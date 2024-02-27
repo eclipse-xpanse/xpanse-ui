f21a09d7c915892be502c3ce78bd1c6e9ad5b471
WangLiNaruto <wl-naruto@outlook.com>
2024-02-27 15:30:20 +0800

    update api

---
+++ src/xpanse-api/generated/models/ReviewRegistrationRequest.ts
@@ -0,0 +1,28 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * SPDX-FileCopyrightText: Huawei Inc.
+ */
+
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type ReviewRegistrationRequest = {
+    /**
+     * The result of review registration.
+     */
+    reviewResult: ReviewRegistrationRequest.reviewResult;
+    /**
+     * The comment of review registration.
+     */
+    reviewComment?: string;
+};
+export namespace ReviewRegistrationRequest {
+    /**
+     * The result of review registration.
+     */
+    export enum reviewResult {
+        APPROVED = 'approved',
+        REJECTED = 'rejected',
+    }
+}
