ffb18200cd52c57f8050c1f1078d2808c8599ca9
WangLiNaruto <wl-naruto@outlook.com>
2024-07-04 15:20:13 +0800

    Update error alert when AZ loading fails and AZ is not mandatory field

--- src/components/content/order/common/availabilityzone/AvailabilityZoneError.tsx
+++ src/components/content/order/common/availabilityzone/AvailabilityZoneError.tsx
@@ -6,56 +6,86 @@
 import { Alert, Button } from 'antd';
 import React from 'react';
 import errorAlertStyles from '../../../../../styles/error-alert.module.css';
+import serviceOrderStyles from '../../../../../styles/service-order.module.css';
 import { ApiError, Response } from '../../../../../xpanse-api/generated';
 import { convertStringArrayToUnorderedList } from '../../../../utils/generateUnorderedList';
 
 export function AvailabilityZoneError({
+    isAvailabilityZoneMandatory,
     retryRequest,
     error,
 }: {
+    isAvailabilityZoneMandatory: boolean;
     retryRequest: () => void;
     error: Error;
 }): React.JSX.Element {
-    if (error instanceof ApiError && error.body && typeof error.body === 'object' && 'details' in error.body) {
-        const response: Response = error.body as Response;
-        return (
-            <Alert
-                message={response.resultType.valueOf()}
-                description={convertStringArrayToUnorderedList(response.details)}
-                type={'error'}
-                closable={false}
-                action={
-                    <Button
-                        className={errorAlertStyles.tryAgainBtnInAlertClass}
-                        size='small'
-                        type='primary'
-                        onClick={retryRequest}
-                        danger={true}
-                    >
-                        Retry Request
-                    </Button>
-                }
-            />
-        );
+    if (isAvailabilityZoneMandatory) {
+        if (error instanceof ApiError && error.body && typeof error.body === 'object' && 'details' in error.body) {
+            const response: Response = error.body as Response;
+            return (
+                <Alert
+                    message={response.resultType.valueOf()}
+                    description={convertStringArrayToUnorderedList(response.details)}
+                    type={'error'}
+                    closable={false}
+                    action={
+                        <Button
+                            className={errorAlertStyles.tryAgainBtnInAlertClass}
+                            size='small'
+                            type='primary'
+                            onClick={retryRequest}
+                            danger={true}
+                        >
+                            Retry Request
+                        </Button>
+                    }
+                />
+            );
+        } else {
+            return (
+                <Alert
+                    message='Fetching Availability Regions Failed'
+                    description={error.message}
+                    type={'error'}
+                    closable={false}
+                    action={
+                        <Button
+                            className={errorAlertStyles.tryAgainBtnInAlertClass}
+                            size='small'
+                            type='primary'
+                            onClick={retryRequest}
+                            danger={true}
+                        >
+                            Retry Request
+                        </Button>
+                    }
+                />
+            );
+        }
     } else {
         return (
-            <Alert
-                message='Fetching Availability Regions Failed'
-                description={error.message}
-                type={'error'}
-                closable={false}
-                action={
-                    <Button
-                        className={errorAlertStyles.tryAgainBtnInAlertClass}
-                        size='small'
-                        type='primary'
-                        onClick={retryRequest}
-                        danger={true}
-                    >
-                        Retry Request
-                    </Button>
-                }
-            />
+            <p className={serviceOrderStyles.orderFormAlert}>
+                <Alert
+                    message='Fetching Availability Regions Failed'
+                    description={
+                        ' This Availability Zone is not a mandatory field. You can proceed without this, ' +
+                        'and the service will be deployed on one of the availability zones of the regions by default.'
+                    }
+                    type={'warning'}
+                    closable={false}
+                    action={
+                        <Button
+                            className={errorAlertStyles.tryAgainBtnInAlertClass}
+                            size='small'
+                            type='primary'
+                            onClick={retryRequest}
+                            danger={true}
+                        >
+                            Retry Request
+                        </Button>
+                    }
+                />
+            </p>
         );
     }
 }
