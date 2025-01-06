61720e859b3cd23b9193b20265a34ca763638768
WangLiNaruto <122504203+WangLiNaruto@users.noreply.github.com>
2025-01-06 15:54:14 +0800

    Add service template history view to the ISV view (#1402)

---
+++ src/components/content/catalog/services/history/ServiceTemplateHistory.tsx
@@ -0,0 +1,64 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * SPDX-FileCopyrightText: Huawei Inc.
+ */
+
+import { HistoryOutlined } from '@ant-design/icons';
+import { Button, Modal } from 'antd';
+import React, { useState } from 'react';
+import catalogStyles from '../../../../../styles/catalog.module.css';
+import { ServiceTemplateDetailVo, ServiceTemplateRequestHistory } from '../../../../../xpanse-api/generated';
+import ServiceTemplateHistoryTable from './ServiceTemplateHistoryTable';
+import useServiceTemplateHistoryQuery from './useServiceTemplateHistoryQuery';
+
+function ServiceTemplateHistory({
+    serviceTemplateDetailVo,
+}: {
+    serviceTemplateDetailVo: ServiceTemplateDetailVo;
+}): React.JSX.Element {
+    const serviceTemplateHistoryQuery = useServiceTemplateHistoryQuery(serviceTemplateDetailVo.serviceTemplateId);
+    const [isServiceTemplateHistoryModalOpen, setIsServiceTemplateHistoryModalOpen] = useState(false);
+
+    let serviceTemplateRequestHistoryList: ServiceTemplateRequestHistory[] = [];
+    if (serviceTemplateHistoryQuery.isSuccess) {
+        serviceTemplateRequestHistoryList = serviceTemplateHistoryQuery.data;
+    }
+
+    const handleServiceTemplateHistoryOpenModal = () => {
+        setIsServiceTemplateHistoryModalOpen(true);
+    };
+
+    const handleServiceTemplateHistoryModalClose = () => {
+        setIsServiceTemplateHistoryModalOpen(false);
+    };
+
+    return (
+        <div>
+            {isServiceTemplateHistoryModalOpen && serviceTemplateRequestHistoryList.length > 0 ? (
+                <Modal
+                    title={'Service Template History'}
+                    width={1600}
+                    footer={null}
+                    open={isServiceTemplateHistoryModalOpen}
+                    onCancel={handleServiceTemplateHistoryModalClose}
+                >
+                    <ServiceTemplateHistoryTable data={serviceTemplateRequestHistoryList} />
+                </Modal>
+            ) : null}
+            {serviceTemplateHistoryQuery.isSuccess && serviceTemplateHistoryQuery.data.length > 0 ? (
+                <Button
+                    icon={<HistoryOutlined />}
+                    type='primary'
+                    onClick={() => {
+                        handleServiceTemplateHistoryOpenModal();
+                    }}
+                    className={catalogStyles.catalogManageBtnClass}
+                >
+                    History
+                </Button>
+            ) : null}
+        </div>
+    );
+}
+
+export default ServiceTemplateHistory;
---
+++ src/components/content/catalog/services/history/ServiceTemplateHistoryTable.tsx
@@ -0,0 +1,167 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * SPDX-FileCopyrightText: Huawei Inc.
+ */
+
+import { InfoCircleOutlined } from '@ant-design/icons';
+import { Alert, Button, Modal, Skeleton, Table, Tag } from 'antd';
+import { ColumnsType } from 'antd/es/table';
+import React, { useState } from 'react';
+import catalogStyles from '../../../../../styles/catalog.module.css';
+import { ServiceTemplateRequestHistory } from '../../../../../xpanse-api/generated';
+import { isHandleKnownErrorResponse } from '../../../common/error/isHandleKnownErrorResponse';
+import DisplayOclData from '../../../common/ocl/DisplayOclData';
+import useRequestedServiceTemplateQuery from './useRequestedServiceTemplateQuery';
+
+function ServiceTemplateHistoryTable({ data }: { data: ServiceTemplateRequestHistory[] }): React.JSX.Element {
+    const [isRequestedServiceTemplateQuery, setIsRequestedServiceTemplateQuery] = useState<boolean>(false);
+    const [serviceTemplateRequestId, setServiceTemplateRequestHistoryId] = useState<string | undefined>(undefined);
+    const requestedServiceTemplateQuery = useRequestedServiceTemplateQuery(serviceTemplateRequestId);
+
+    const columns: ColumnsType<ServiceTemplateRequestHistory> = [
+        {
+            title: 'RequestId',
+            dataIndex: 'requestId',
+            align: 'center',
+        },
+        {
+            title: 'RequestType',
+            dataIndex: 'requestType',
+            align: 'center',
+            render: (_text, record) => {
+                if (record.requestType === 'register') {
+                    return <Tag color='default'>{record.requestType}</Tag>;
+                } else if (record.requestType === 'republish') {
+                    return <Tag color='success'>{record.requestType}</Tag>;
+                } else if (record.requestType === 'unpublish') {
+                    return <Tag color='error'>{record.requestType}</Tag>;
+                } else {
+                    return <Tag color='processing'>{record.requestType}</Tag>;
+                }
+            },
+        },
+        {
+            title: 'Status',
+            dataIndex: 'status',
+            align: 'center',
+            render: (_text, record) => {
+                if (record.status === 'in-review') {
+                    return <Tag color='#ffa366'>{record.status}</Tag>;
+                } else if (record.status === 'accepted') {
+                    return <Tag color='#87d068'>{record.status}</Tag>;
+                } else if (record.status === 'rejected') {
+                    return <Tag color='#ff6666'>{record.status}</Tag>;
+                } else {
+                    return <Tag color='default'>{record.status}</Tag>;
+                }
+            },
+        },
+        {
+            title: 'ReviewComment',
+            dataIndex: 'reviewComment',
+            align: 'center',
+        },
+        {
+            title: 'CreatedTime',
+            dataIndex: 'createTime',
+            align: 'center',
+        },
+        {
+            title: 'LastModifiedTime',
+            dataIndex: 'lastModifiedTime',
+            align: 'center',
+        },
+        {
+            title: 'Service Template',
+            dataIndex: 'service template',
+            render: (_text: string, record) => {
+                return (
+                    <>
+                        <Button
+                            type='primary'
+                            icon={<InfoCircleOutlined />}
+                            onClick={() => {
+                                handleRequestedServiceTemplateOpenModal(record);
+                            }}
+                        >
+                            request template
+                        </Button>
+                    </>
+                );
+            },
+            align: 'center',
+        },
+    ];
+
+    const handleRequestedServiceTemplateOpenModal = (record: ServiceTemplateRequestHistory) => {
+        setIsRequestedServiceTemplateQuery(true);
+        setServiceTemplateRequestHistoryId(record.requestId);
+    };
+
+    const handleRequestedServiceTemplateModalClose = () => {
+        setIsRequestedServiceTemplateQuery(false);
+        setServiceTemplateRequestHistoryId(undefined);
+    };
+
+    return (
+        <div>
+            {requestedServiceTemplateQuery.isLoading ? (
+                <Modal
+                    title={'Requested Service Template'}
+                    width={1600}
+                    footer={null}
+                    open={isRequestedServiceTemplateQuery}
+                    onCancel={handleRequestedServiceTemplateModalClose}
+                >
+                    <Skeleton active />
+                </Modal>
+            ) : requestedServiceTemplateQuery.isSuccess ? (
+                <Modal
+                    title={'Requested Service Template'}
+                    width={1600}
+                    footer={null}
+                    open={isRequestedServiceTemplateQuery}
+                    onCancel={handleRequestedServiceTemplateModalClose}
+                >
+                    <DisplayOclData ocl={requestedServiceTemplateQuery.data} />
+                </Modal>
+            ) : requestedServiceTemplateQuery.isError ? (
+                <div className={catalogStyles.requestedServiceTemplateHistoryError}>
+                    {isHandleKnownErrorResponse(requestedServiceTemplateQuery.error) ? (
+                        <Alert
+                            message={
+                                <>
+                                    Request Service Template with requestId <strong>{serviceTemplateRequestId}</strong>{' '}
+                                    error
+                                </>
+                            }
+                            description={String(requestedServiceTemplateQuery.error.body.details)}
+                            showIcon
+                            type={'error'}
+                            closable={true}
+                            onClose={handleRequestedServiceTemplateModalClose}
+                        />
+                    ) : (
+                        <Alert
+                            message={
+                                <>
+                                    Request Service Template with requestId <strong>{serviceTemplateRequestId}</strong>{' '}
+                                    error
+                                </>
+                            }
+                            description={requestedServiceTemplateQuery.error.message}
+                            showIcon
+                            type={'error'}
+                            closable={true}
+                            onClose={handleRequestedServiceTemplateModalClose}
+                        />
+                    )}
+                </div>
+            ) : null}
+
+            <Table columns={columns} dataSource={data} rowKey={'id'} />
+        </div>
+    );
+}
+
+export default ServiceTemplateHistoryTable;
