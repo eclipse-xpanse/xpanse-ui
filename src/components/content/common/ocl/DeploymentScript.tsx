8350c62e314b341bd70f9ad11839a4563ab92ce2
swaroop.ar <swaroop.akulu.raghupathy@huawei.com>
2025-04-17 12:26:01 +0200

    generate API changes for helm deployer tool

--- src/components/content/common/ocl/DeploymentScript.tsx
+++ src/components/content/common/ocl/DeploymentScript.tsx
@@ -10,12 +10,12 @@ import oclDisplayStyles from '../../../../styles/ocl-display.module.css';
 import { Deployment } from '../../../../xpanse-api/generated';
 
 export function DeploymentScriptText({ deployment }: { deployment: Deployment }): React.JSX.Element {
-    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-    if (deployment) {
+    if (deployment.terraformDeployment) {
         const yamlDocument = new YAML.Document();
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-expect-error
-        yamlDocument.contents = deployment.scriptFiles ?? deployment.scriptsRepo;
+        yamlDocument.contents =
+            deployment.terraformDeployment.scriptFiles ?? deployment.terraformDeployment.scriptsRepo;
         return (
             <Popover
                 content={<pre className={oclDisplayStyles.oclDeploymentScript}>{yamlDocument.toString()}</pre>}
