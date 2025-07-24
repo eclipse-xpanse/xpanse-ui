5917ace758942a7266b73ecad58f556418aaab60
swaroop.ar <swaroop.akulu.raghupathy@huawei.com>
2025-07-24 16:11:00 +0200

    add option to provide a custom base URI

--- src/index.tsx
+++ src/index.tsx
@@ -10,12 +10,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
+import { env } from './config/config.ts';
 import './styles/index.css';
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
-    <BrowserRouter>
+    <BrowserRouter basename={env.VITE_APP_BASE_URI}>
         <React.StrictMode>
             <App />
         </React.StrictMode>
